1c964298a5cd31644138d3ab0cf7312dd85763f1
stirelshka8 <stirelshka8@gmail.com>
2023-01-24 12:16:56 +0300

    Its work

--- logistic/views.py
+++ logistic/views.py
@@ -3,14 +3,18 @@ from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
+from rest_framework.filters import SearchFilter
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
-    queryset = Stock.objects.all()
+    queryset = Stock.objects.prefetch_related('positions')
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['products__title', 'products__description']
